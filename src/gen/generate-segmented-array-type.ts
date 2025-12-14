13bbc381c56594012a7df34f3862d8bee1a2fe51
2qp <56475797+2qp@users.noreply.github.com>
2025-12-14 21:21:01 +0530

    feat(gen): add `generateSegmentedArrayType`

---
+++ src/gen/generate-segmented-array-type.ts
@@ -0,0 +1,62 @@
+import { createElementType } from "@core/create-element-type";
+import { createSegmentDefs } from "@core/create-segment-defs";
+import { createSegmentRefs } from "@core/create-segment-refs";
+import { getArrayTypeString } from "@core/get-array-type-string";
+import { chunkArray } from "@utils/chunk-array";
+import { join } from "@utils/join";
+import { sortMixed } from "@utils/sort";
+
+import type { GeneratedDefs } from "@/types/def.types";
+import type { ElementBase } from "@/types/field.types";
+import type { Primitive } from "@/types/gen.types";
+import type { TypeGenConfig } from "@/types/gen-config.types";
+
+type GenerateSegmentedArrayTypeParams<T extends Primitive, TBase extends ElementBase, TName extends string> = {
+	values: Set<T>;
+	baseType: TBase;
+	typeName: TName;
+	config: TypeGenConfig;
+};
+
+type GenerateSegmentedArrayTypeType = <T extends Primitive, TBase extends ElementBase, TName extends string>(
+	params: GenerateSegmentedArrayTypeParams<T, TBase, TName>,
+) => GeneratedDefs<T, TName, TBase>;
+
+const generateSegmentedArrayType: GenerateSegmentedArrayTypeType = ({ values, baseType, typeName, config }) => {
+	//
+
+	const filteredValues = [...values].filter((v) => typeof v === baseType);
+
+	const sortedValues = sortMixed(filteredValues);
+
+	if (sortedValues.length === 0) {
+		const elementType = baseType;
+
+		const typeDef = getArrayTypeString({ elementType, readonly: config.useReadonlyArrays });
+
+		return {
+			typeDef,
+			segmentDefs: [],
+		};
+	}
+
+	const chunks = chunkArray(sortedValues, config.itemsPerSegment);
+
+	const segmentDefs = createSegmentDefs({ chunks, typeName });
+
+	const segmentRefs = createSegmentRefs({ chunks, typeName });
+
+	const combinedElementType = join(segmentRefs, " | " as const);
+
+	const elementType = createElementType({ combined: combinedElementType, base: baseType, config });
+
+	const typeDef = getArrayTypeString({ elementType, readonly: config.useReadonlyArrays });
+
+	return {
+		typeDef,
+		segmentDefs,
+	};
+};
+
+export { generateSegmentedArrayType };
+export type { GenerateSegmentedArrayTypeParams, GenerateSegmentedArrayTypeType };
