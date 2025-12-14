19d3de9abb78616aaa677b7377153df18ac71f98
2qp <56475797+2qp@users.noreply.github.com>
2025-12-14 22:06:55 +0530

    feat(gen): add `generateLiteralType`

---
+++ src/gen/generate-literal-type.ts
@@ -0,0 +1,88 @@
+import { createElementType } from "@core/create-element-type";
+import { createSegmentDefs } from "@core/create-segment-defs";
+import { createSegmentRefs } from "@core/create-segment-refs";
+import { getArrayTypeString } from "@core/get-array-type-string";
+import { chunkArray } from "@utils/chunk-array";
+import { join } from "@utils/join";
+import { replacer } from "@utils/replacer";
+import { shouldSplitTypes } from "@utils/should-split-types";
+import { sortMixed } from "@utils/sort";
+
+import type { GeneratedDefs } from "@/types/def.types";
+import type { ElementBase } from "@/types/field.types";
+import type { Primitive } from "@/types/gen.types";
+import type { TypeGenConfig } from "@/types/gen-config.types";
+
+type GenerateLiteralTypeParams<T extends Primitive, TBase extends ElementBase, TName extends string> = {
+	values: Set<T>;
+	baseType: TBase;
+	typeName?: TName | undefined;
+	config: TypeGenConfig;
+};
+
+type GenerateLiteralTypeType = <T extends Primitive, TBase extends ElementBase, TName extends string>(
+	params: GenerateLiteralTypeParams<T, TBase, TName>,
+) => GeneratedDefs<T, TName, TBase>;
+
+const generateLiteralType: GenerateLiteralTypeType = ({ values, baseType, config, typeName = undefined }) => {
+	//
+
+	const filteredValues = [...values].filter((v) => typeof v === baseType);
+
+	const sortedValues = sortMixed(filteredValues);
+
+	//
+	if (sortedValues.length === 0) {
+		const elementType = baseType;
+
+		const typeDef = getArrayTypeString({ elementType, readonly: config.useReadonlyArrays });
+
+		return {
+			typeDef,
+			segmentDefs: [],
+		};
+	}
+
+	const shouldSplit = shouldSplitTypes(config, sortedValues) && typeName;
+
+	if (!shouldSplit) {
+		//
+
+		const literals = sortedValues.map((values) => {
+			const template = `${values}` as const;
+
+			const escapedQuotes = replacer(template, '"' as const, '\\"' as const);
+			const escapedQuotesAndNewlines = replacer(escapedQuotes, "\n" as const, "\\n" as const);
+
+			return `"${escapedQuotesAndNewlines}"` as const;
+		});
+
+		const joinedLits = join(literals, " | " as const);
+
+		const typeDef = createElementType({ combined: joinedLits, base: baseType, config });
+
+		return {
+			typeDef,
+			segmentDefs: [],
+		};
+	}
+
+	const chunks = chunkArray(sortedValues, config.itemsPerSegment);
+
+	const segmentDefs = createSegmentDefs({ chunks, typeName });
+	const segmentRefs = createSegmentRefs({ chunks, typeName });
+
+	const combinedType = join(segmentRefs, " | " as const);
+
+	const typeDef = createElementType({ combined: combinedType, base: baseType, config });
+
+	// const typeDef = getArrayTypeString({ elementType, readonly: config.allowFlexibleTypes });
+
+	return {
+		typeDef,
+		segmentDefs,
+	};
+};
+
+export { generateLiteralType };
+export type { GenerateLiteralTypeParams, GenerateLiteralTypeType };
