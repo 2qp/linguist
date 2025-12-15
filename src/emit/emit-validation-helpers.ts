fcb3cf553fa6624c22aad11ef740f4485c1cac82
2qp <56475797+2qp@users.noreply.github.com>
2025-12-15 20:38:44 +0530

    refactor(emit): rewrite `isValidLanguageName`, `getLanguage` as const arrow function

--- src/emit/emit-validation-helpers.ts
+++ src/emit/emit-validation-helpers.ts
@@ -1,14 +1,14 @@
 const emitValidationHelpers = (languageName: string) =>
 	[
 		`// Validation helpers\n `,
-		`export function isValidLanguageName(name: string): name is ${languageName} {\n`,
+		`export const isValidLanguageName = (name: string): name is ${languageName} => {\n`,
 		` return name in ({} as unknown as Languages);`,
 		`}\n\n`,
 
 		"\n",
-		`export function getLanguage<Name extends ${languageName}>(name: Name): GetLanguage<Name> | undefined {\n`,
+		`export const getLanguage = <Name extends ${languageName}>(_name: Name): GetLanguage<Name> | undefined => {\n`,
 		` // impl... \n`,
-		`return undefined as any;\n`,
+		`return undefined;\n`,
 		`}\n\n`,
 	].join("");
 
