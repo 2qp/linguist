690c4f66b4d7ef734754cf8dae9fc990ed6151c7
2qp <56475797+2qp@users.noreply.github.com>
2025-12-15 09:34:54 +0530

    feat(gen): add `generateFieldType`
    - delegates generators based on the type

---
+++ src/gen/generate-field-type.ts
@@ -0,0 +1,89 @@
+import { generateArrayType } from "./generate-array-type";
+import { generateLiteralType } from "./generate-literal-type";
+
+import type { GeneratedDefs } from "@/types/def.types";
+import type { ElementBase, FieldAnalysis } from "@/types/field.types";
+import type { Primitive } from "@/types/gen.types";
+import type { TypeGenConfig } from "@/types/gen-config.types";
+
+type GenerateFieldTypeParams<TName extends string, _TBase extends ElementBase, TUnique extends Primitive> = {
+	field: string;
+	stats: FieldAnalysis<TUnique>;
+	typeName?: TName | undefined;
+	config: TypeGenConfig;
+};
+
+type GenerateFieldTypeType = <TName extends string, TBase extends ElementBase, TUnique extends Primitive>(
+	params: GenerateFieldTypeParams<TName, TBase, TUnique>,
+) => GeneratedDefs<TUnique, TName, ElementBase>;
+
+const generateFieldType: GenerateFieldTypeType = ({ stats, typeName = undefined, config }) => {
+	//
+
+	if (stats.isArray) {
+		return generateArrayType({ stats, config, typeName });
+	}
+
+	//
+	if (stats.isBoolean) {
+		if (stats.shouldBeLiteral) {
+			//
+
+			const values = Array.from(stats.uniqueValues);
+			if (values.length === 1 && values[0] !== undefined) {
+				const bool = values[0];
+
+				if (bool) return { typeDef: `true`, segmentDefs: [] };
+
+				return { typeDef: `false`, segmentDefs: [] };
+			}
+
+			return {
+				typeDef: "boolean",
+				segmentDefs: [],
+			};
+		}
+
+		return {
+			typeDef: "boolean",
+			segmentDefs: [],
+		};
+	}
+
+	//
+	if (stats.isNumber) {
+		if (stats.shouldBeLiteral) {
+			return generateLiteralType({ values: stats.uniqueValues, baseType: "number", config, typeName });
+		}
+		return {
+			typeDef: "number",
+			segmentDefs: [],
+		};
+	}
+
+	//
+	if (stats.isString) {
+		if (stats.shouldBeLiteral) {
+			return generateLiteralType({ values: stats.uniqueValues, baseType: "string", config, typeName });
+		}
+
+		const typeDef = config.allowFlexibleTypes ? ("string | (string & {})" as const) : ("string" as const);
+
+		return {
+			typeDef,
+			segmentDefs: [],
+		};
+	}
+
+	const elementType = "any" as const;
+
+	// mixed or object
+	// mmm will handle later / recursive
+	return {
+		typeDef: elementType,
+		segmentDefs: [],
+	};
+};
+
+export { generateFieldType };
+export type { GenerateFieldTypeParams, GenerateFieldTypeType };
