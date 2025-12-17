ba3654bf7b81e3b0f063609fe1fffa0d9fa1558c
2qp <56475797+2qp@users.noreply.github.com>
2025-12-17 10:42:18 +0530

    feat(infra/loaders): add `data.config` on `configLoader`

--- src/infra/loaders/config-loader.ts
+++ src/infra/loaders/config-loader.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "node:url";
 
 import type { Config } from "@/types/config.types";
 import type { CoreConfigFile } from "@/types/core.types";
+import type { DataConfigFile } from "@/types/data.types";
 import type { TypeGenConfigFile } from "@/types/gen-config.types";
 
 // type ConfigLoaderParams = {};
@@ -21,17 +22,21 @@ const configLoader: ConfigLoaderType = async () => {
 
 	const corePath = join(path, "core.config.yaml");
 	const typeGenPath = join(path, "type.config.yaml");
+	const dataPath = join(path, "data.config.yaml");
 
 	const coreFile = await readFile(corePath, "utf8");
 	const typeGenFile = await readFile(typeGenPath, "utf8");
+	const dataFile = await readFile(dataPath, "utf8");
 
 	const core = yamlLoader<CoreConfigFile>({ str: coreFile });
 	const typegen = yamlLoader<TypeGenConfigFile>({ str: typeGenFile });
+	const data = yamlLoader<DataConfigFile>({ str: dataFile });
 
 	if (!core) throw Error("Unable to load core config");
 	if (!typegen) throw Error("Unable to load typegen config");
+	if (!data) throw Error("Unable to load data config");
 
-	return { ...core, ...typegen };
+	return { ...core, ...typegen, ...data };
 };
 
 export { configLoader };
