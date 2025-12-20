8d2915b7f4fc6c3f96198c0a23456e779d7aa0c1
2qp <56475797+2qp@users.noreply.github.com>
2025-12-20 20:55:30 +0530

    feat(gen): `generateTypes`

---
+++ src/gen/generate-types.ts
@@ -0,0 +1,46 @@
+import { generateDynamicTypes } from "./generate-dynamic-types";
+import { join } from "node:path";
+import { getFile } from "@services/fetch/get-file";
+import { ensureDir } from "@utils/ensure-dir";
+import { resolvePath } from "@utils/resolve-path";
+import { writeFile } from "@utils/write-file";
+import { configLoader } from "@/infra/loaders/config-loader";
+import { yamlLoader } from "@/infra/loaders/yaml-loader";
+
+import type { LanguageData } from "@/types/lang.types";
+
+type GenerateTypesParams = {};
+
+type GenerateTypesType = (params: GenerateTypesParams) => void;
+
+const generateTypes: GenerateTypesType = async () => {
+	//
+
+	const config = await configLoader();
+
+	const fileStr = await getFile<string>(config.core.url, "text");
+
+	const yamlPath = config.core.url;
+
+	const outputDir = resolvePath(config.type.paths.outputDir);
+
+	try {
+		console.info(`Reading: ${yamlPath}...\n`);
+
+		const data = yamlLoader<LanguageData>({ str: fileStr });
+
+		const typesOutput = generateDynamicTypes({ config: config, data });
+
+		await ensureDir(outputDir);
+		const outputPath = join(outputDir, config.type.out.fileName);
+		await writeFile({ content: typesOutput, filePath: outputPath });
+
+		// mmmaybe use `ora spinner`
+		console.info(`Types generated to: ${outputPath}\n`);
+	} catch (error) {
+		console.error("Error generating types:", error);
+	}
+};
+
+export { generateTypes };
+export type { GenerateTypesParams, GenerateTypesType };
