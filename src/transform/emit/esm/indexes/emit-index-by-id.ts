c45cecb0763209a7e9a52fd520e87f52a76f58df
2qp <56475797+2qp@users.noreply.github.com>
2025-12-25 23:06:37 +0530

    feat(transform/esm): add `typeImports`, `typeEntries` on `emitIndexById`
    - add `FallbackForUnknownKeys<Language | undefined>`

--- src/transform/emit/esm/indexes/emit-index-by-id.ts
+++ src/transform/emit/esm/indexes/emit-index-by-id.ts
@@ -20,6 +20,18 @@ const emitIndexById: IndexEmitterType = ({ languages, config }): string => {
 		//
 	});
 
+	const types = (Object.entries(languages) as Entries<Languages>).map(([name, language]) => {
+		//
+
+		const norm = normalizeName(name);
+
+		const entry = `  "${language.language_id}": ${norm.typeName},` as const;
+
+		return entry;
+
+		//
+	});
+
 	const imports = (Object.entries(languages) as Entries<Languages>)
 		.filter((lang) => lang[1].language_id !== undefined)
 		.map(([name]) => {
@@ -30,22 +42,41 @@ const emitIndexById: IndexEmitterType = ({ languages, config }): string => {
 			return ` import { ${norm.varName} } from "${removeTrailingSlash(config.data.paths.typesDir)}/${type}/${norm.fileName}";` as const;
 		});
 
+	const manualTypeImports = [
+		`import type { Language, FallbackForUnknownKeys } from "${config.type.aliases.outputDir}/${config.type.out.fileNameNoExt}"`,
+	];
+
+	const typeImports = (Object.entries(languages) as Entries<Languages>).map(([name]) => {
+		const norm = normalizeName(name);
+		const langData = languages[name];
+		const type = langData?.type || "programming";
+		return ` import type { ${norm.typeName} } from "${removeTrailingSlash(config.data.paths.typesDir)}/${type}/${norm.fileName}";`;
+	});
+
 	const joinedResult = result.join("\n");
 	const joinedImports = imports.join("\n");
+	const joinedTypeImports = typeImports.join("\n");
+	const joinedManualTypeImports = manualTypeImports.join("\n");
+	const joinedTypeEntries = types.join("\n");
 
 	// later with ts compiler api
 	return [
 		`${joinedImports}`,
 		"\n\n",
-		"const byId = {",
+		`${joinedTypeImports}`,
+		"\n\n",
+		`${joinedManualTypeImports}`,
+		"\n\n",
+		"const byId : ById = {",
 		"\n",
 		`${joinedResult}`,
 		"\n",
 		"} as const;",
 		"\n\n",
-		"export { byId };",
+		`type ById = {\n${joinedTypeEntries}\n} & FallbackForUnknownKeys<Language | undefined>;\n`,
 		"\n\n",
-		"export type ById = typeof byId;",
+		"export { byId };\n",
+		"export type { ById };",
 		"\n",
 	].join("");
 };
