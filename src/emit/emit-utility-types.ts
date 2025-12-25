7c0fed59b300b3aa8c0df8d3181416017fe7b05b
2qp <56475797+2qp@users.noreply.github.com>
2025-12-25 22:16:44 +0530

    feat(emit): add `FallbackForUnknownKeys` on `emitUtilityTypes`
    - could use `ts-morph` to write ts types

--- src/emit/emit-utility-types.ts
+++ src/emit/emit-utility-types.ts
@@ -3,6 +3,10 @@ const emitUtilityTypes = (languageName: string) =>
 		`// Utility types\n`,
 		` export type Languages = Record<${languageName}, Language>;\n`,
 		` export type LanguageField = keyof Language;\n\n`,
+		` export type FallbackForUnknownKeys<T> = {
+			[K in string & { __fallback?: never }]: T;
+			};`,
+		`\n\n`,
 	].join("");
 
 export { emitUtilityTypes };
