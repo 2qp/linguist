67a7ba47b32a30d44bcdf6815aea1f76179d135f
2qp <56475797+2qp@users.noreply.github.com>
2025-12-29 10:13:45 +0530

    feat(transform/accessors): add `getOne`

---
+++ src/transform/accessors/get-one.ts
@@ -0,0 +1,32 @@
+// type GetLangByExtensionParams<T> = {
+// 	// ext: LooseToStrict<Extensions[number]>;
+// 	ext: T;
+// };
+
+// type GetOneOverloaded = {
+// 	<T extends keyof ByExtension>(index: ByExtension, ext: T, _?: "ext"): ByExtension[T];
+// 	<T extends keyof ById>(index: ById, id: T, _?: "id"): ById[T];
+// 	<T extends keyof LanguageToCategory>(index: LanguageToCategory, lang: T, _?: "lng_cat"): LanguageToCategory[T];
+
+// 	<T extends string>(index: ByExtension, ext: T, _?: "ext"): ToObjOne<ByExtension, T, Language[]>;
+// 	<T extends string>(index: ById, id: T, _?: "id"): ToObjOne<ById, T, Language[]>;
+// 	<T extends string>(index: LanguageToCategory, lang: T, _?: "lng_cat"): ToObjOne<LanguageToCategory, T, Type>;
+// };
+
+type GetOneOverloaded = {
+	<I, T extends keyof I>(index: I, ext: T): I[T];
+
+	// <I, T extends string>(index: I, ext: T): ToObjOne<I, T, CombinedPropsFromAny<I>>;
+
+	<I, T extends string>(index: I, ext: T): I[keyof I];
+};
+
+type GetLangByExtensionType = GetOneOverloaded;
+
+const getOne: GetLangByExtensionType = (index: object, ext: string) => {
+	//
+
+	return index[ext as keyof typeof index] || [];
+};
+
+export { getOne };
