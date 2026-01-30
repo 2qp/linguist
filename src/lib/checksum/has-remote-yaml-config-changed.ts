8d2d7a63fe444c0486ce1e8da2b510660fb768b6
2qp <56475797+2qp@users.noreply.github.com>
2026-01-30 22:12:25 +0530

    feat(checksum): add `getSourceHash`
    - add `getSourceHash` on `hasRemoteYamlOrConfigChanged`.

--- src/lib/checksum/has-remote-yaml-config-changed.ts
+++ src/lib/checksum/has-remote-yaml-config-changed.ts
@@ -1,5 +1,6 @@
 import { fetchRemoteYaml } from "./fetch-remote-yaml";
 import { readPrevBuildInfoHash } from "./read-prev-build-info-hash";
+import { getSourceHash } from "@utils/get-source-hash";
 import { hashContent } from "@utils/hash-content";
 import { stringify } from "safe-stable-stringify";
 import { configLoader } from "@/infra/loaders/config-loader";
@@ -20,15 +21,18 @@ const hasRemoteYamlOrConfigChanged: HasRemoteYamlOrConfigChangedType = async () 
 	const yamlStr = await fetchRemoteYaml(configObj.core.url);
 	const yamlObj = await yamlLoader({ str: yamlStr });
 
-	const obj = { yamlObj, configObj };
+	const GENERATOR_HASH = getSourceHash(configObj.core.basis);
+
+	const obj = { yamlObj, configObj, logic: GENERATOR_HASH };
 
 	const currentHash = hashContent(stringify(obj));
 
 	const prevHash = await readPrevBuildInfoHash(configObj.core.build_info_path);
 
 	if (currentHash === prevHash) {
-		console.info("🟡 Remote YAML unchanged - skipping build");
-		console.info("🟡 Config unchanged - skipping build");
+		console.info("🟡 Remote YAML unchanged - skipping generation");
+		console.info("🟡 Config unchanged - skipping generation");
+		console.info(`🟡 Generator unchanged - skipping generation [${configObj.core.basis}]`);
 		return { changed: false };
 	}
 
---
+++ src/utils/get-source-hash.ts
@@ -0,0 +1,36 @@
+import { execSync } from "node:child_process";
+
+import type { CoreConfig } from "@/types/core.types";
+
+type GetSourceHashParams = CoreConfig["basis"];
+
+type GetSourceHashType = (params: GetSourceHashParams) => string | undefined;
+
+const contentSources = {
+	committed: "xargs -I {} git show HEAD:{}",
+	"working-tree": "xargs sha1sum",
+} as const satisfies Record<CoreConfig["basis"], string>;
+
+const getSourceHash: GetSourceHashType = (basis) => {
+	//
+
+	const include = ["src/", "pnpm-lock.yaml"];
+	const exclude = ["src/ui-only-stuff"];
+
+	const includeStr = include.map((p) => `'${p}'`).join(" ");
+	const excludeStr = exclude.map((p) => `grep -v '${p}'`).join(" | ");
+
+	// git history or fs
+	const contentSource = contentSources[basis];
+
+	try {
+		const cmd = `git ls-files ${includeStr} | ${excludeStr} | ${contentSource} | sha1sum`;
+		return execSync(cmd, { encoding: "utf8" }).trim().split(" ")[0];
+	} catch (err) {
+		console.warn("couldn't generate logic hash (git error). falling back to timestamp. | ", err);
+		return `fallback-${Date.now()}`;
+	}
+};
+
+export { getSourceHash };
+export type { GetSourceHashParams, GetSourceHashType };
