b606e1789bdb196f9ba3980ac8656d00bb73cb8d
2qp <56475797+2qp@users.noreply.github.com>
2026-02-06 01:29:33 +0530

    refactor(constants): remove `FIELD_TYPE_MAPPING`

--- src/constants/field-type-mapping.ts
+++
@@ -1,7 +0,0 @@
-import { LANGUAGE_NAME } from "./identifiers";
-
-import type { FieldTypeMapping } from "@gen/utils/get-mapped-field-or-type";
-
-const FIELD_TYPE_MAPPING = [{ field: "name", type: LANGUAGE_NAME }] as const satisfies FieldTypeMapping[];
-
-export { FIELD_TYPE_MAPPING };
