0037f616261c504d926e3a39cddf4b676741a5a5
2qp <56475797+2qp@users.noreply.github.com>
2026-02-07 20:02:08 +0530

    feat(transform/emit): add `name` property on `emitNormalizedAllFlat`
    - add `FallbackForUnknownKeys` on `NormalizedAll` type.

--- src/transform/emit/flat/emit-normalized-all-flat.ts
+++ src/transform/emit/flat/emit-normalized-all-flat.ts
@@ -4,7 +4,7 @@ import { normalizeName } from "@/transform/utils/normalize-name";
 import type { Language, LanguageName } from "@/generated/types";
 import type { FlatEmitterType } from "./types";
 
-const emitNormalizedAllFlat: FlatEmitterType = ({ languages }) => {
+const emitNormalizedAllFlat: FlatEmitterType = ({ config, languages }) => {
 	//
 
 	const normalized: Record<string, Language> = {};
@@ -29,17 +29,36 @@ const emitNormalizedAllFlat: FlatEmitterType = ({ languages }) => {
 		const value = languages[key];
 		if (!value) continue;
 
+		const nameAttachedValue = { ...value, name: key };
+
 		seenKeys.add(normalizedKey);
-		normalized[normalizedKey] = value;
+		normalized[normalizedKey] = nameAttachedValue;
 	}
 
 	const content = stringify(normalized, null, 2);
 
-	const name = "normalizedAll" as const;
+	const manualTypeImports = [
+		`import type { Language, FallbackForUnknownKeys } from "${config.type.aliases.outputDir}/${config.type.out.fileNameNoExt}"` as const,
+	].join("\n");
 
-	const statements = ([`const ${name} = ${content} as const;`, "\n\n", `export { ${name} };`, "\n\n"] as const).join(
-		"",
-	);
+	const name = "normalizedAll" as const;
+	const typeName = "NormalizedAll" as const;
+
+	const statements = (
+		[
+			manualTypeImports,
+			"\n\n",
+			`const _${name} = ${content} as const;`,
+			"\n\n",
+			`const ${name}: typeof _${name} & FallbackForUnknownKeys<Language | undefined> = _${name};`,
+			"\n\n",
+			`type ${typeName} = typeof ${name};`,
+			"\n\n",
+			`export { ${name} };`,
+			"\n",
+			`export type { ${typeName} };`,
+		] as const
+	).join("");
 
 	return statements;
 };
