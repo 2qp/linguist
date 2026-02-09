4dc132dacc920ca97773cf0736c0c5570cd7a876
2qp <56475797+2qp@users.noreply.github.com>
2026-02-09 17:54:37 +0530

    perf(utils): add optimized `JoinRelaxed` type on `join`
    - add optimizations for better recursion.
    - add explicit handling for single and two-element arrays.
    - add infinite recursion prevension with tuple decomposition.

--- src/utils/join.ts
+++ src/utils/join.ts
@@ -2,13 +2,21 @@ type JoinType = <T extends readonly string[], TSep extends string>(arr: T, sep: 
 
 const join: JoinType = (arr, sep) => arr.join(sep) as JoinRelaxed<typeof arr, typeof sep>;
 
-type JoinRelaxed<T extends readonly string[], TSep extends string, Result extends string = ""> = T extends readonly []
-	? Result
-	: T extends readonly [infer First extends string, ...infer Rest extends readonly string[]]
-		? JoinRelaxed<Rest, TSep, `${Result}${Result extends "" ? "" : TSep}${First}`>
-		: T extends ReadonlyArray<infer Element extends string>
-			? `${Element}${TSep}${Element}`
-			: string;
+type JoinRelaxed<T extends readonly string[], TSep extends string> = T extends readonly []
+	? ""
+	: T extends readonly [
+				infer First extends string,
+				infer Second extends string,
+				...infer Rest extends readonly string[],
+			]
+		? Rest extends []
+			? `${First}${TSep}${Second}`
+			: `${First}${TSep}${JoinRelaxed<[Second, ...Rest], TSep>}`
+		: T extends readonly [infer Single extends string]
+			? Single
+			: T extends ReadonlyArray<infer Element extends string>
+				? `${Element}${TSep}${Element}`
+				: string;
 
 export { join };
 export type { JoinType };
