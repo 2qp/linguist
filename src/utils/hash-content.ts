6aafcaf6d1f372eced8a4715421c3acdb901c2f5
2qp <56475797+2qp@users.noreply.github.com>
2026-02-19 23:02:16 +0530

    refactor(utils): add `BinaryLike` type on `hashContent` param

--- src/utils/hash-content.ts
+++ src/utils/hash-content.ts
@@ -1,5 +1,7 @@
 import { createHash } from "node:crypto";
 
-const hashContent = (content: string): string => createHash("sha256").update(content).digest("hex");
+import type { BinaryLike } from "node:crypto";
+
+const hashContent = (content: BinaryLike): string => createHash("sha256").update(content).digest("hex");
 
 export { hashContent };
