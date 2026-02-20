2c480de0e8270631e7ea3ad81d2e88df6ba7450c
2qp <56475797+2qp@users.noreply.github.com>
2026-02-21 01:28:39 +0530

    fix(emit): add `sort` on `export` statements of `emitTypesSection`

--- src/emit/emit-types-sections.ts
+++ src/emit/emit-types-sections.ts
@@ -4,9 +4,9 @@ import type { Primitive } from "@/types/gen.types";
 
 const emitTypesSection = (types: Map<UID, GeneratedDefs<Primitive, string>>, languageName: string): string =>
 	[...types.keys()]
-		.sort()
 		.filter((tN) => tN !== languageName)
 		.map((uid) => `export type ${types.get(uid)?.type} = ${types.get(uid)?.typeDef};\n`)
+		.sort()
 		.join("");
 
 export { emitTypesSection };
