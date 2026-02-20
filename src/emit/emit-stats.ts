6b5ab417cfd5693bdda5864df4974ccf14822660
2qp <56475797+2qp@users.noreply.github.com>
2026-02-20 21:36:10 +0530

    refactor(emit): add `FieldAnalysisArray` on `emitStats`
    - add `meta` param.
    - rename `map` param to `stats`.
    - remove `totals`, `langs` params.
    - comment out `output_sample_names`.

--- src/emit/emit-stats.ts
+++ src/emit/emit-stats.ts
@@ -1,28 +1,28 @@
 import { getMappedFieldOrType } from "@gen/utils/get-mapped-field-or-type";
 
+import type { Meta } from "@core/create-meta";
 import type { UID } from "@/types/branded.types";
 import type { Config } from "@/types/config.types";
 import type { GeneratedDefs } from "@/types/def.types";
-import type { FieldAnalysisMap } from "@/types/field.types";
+import type { FieldAnalysisArray } from "@/types/field.types";
 import type { Primitive } from "@/types/gen.types";
 
 type EmitStatsParams = {
-	map: FieldAnalysisMap;
+	stats: FieldAnalysisArray;
 	types: Map<UID, GeneratedDefs<Primitive, string>>;
 	config: Config;
-	totals: { total: number; size: number };
-	langs: string[];
+	meta: Meta;
 };
 
 type EmitStatsType = (params: EmitStatsParams) => string;
 
-const emitStats: EmitStatsType = ({ map, types, config, totals, langs }) => {
+const emitStats: EmitStatsType = ({ stats, types, config, meta }) => {
 	//
 
-	const stats = [...map]
+	stats
 		.map(([field, stats]) => {
 			//
-			const usagePercent = ((stats.languagesUsing / totals.total) * 100).toFixed(1);
+			const usagePercent = ((stats.languagesUsing / meta.languageCount) * 100).toFixed(1);
 			const namedType = [...types.keys()].find((tName) => {
 				//
 
@@ -36,7 +36,7 @@ const emitStats: EmitStatsType = ({ map, types, config, totals, langs }) => {
 				return tName.toLowerCase() === remappedField.value.toLowerCase().replace(/_/g, "");
 			});
 
-			const output_field = `// ${field}: used in ${stats.languagesUsing}/${totals.total} (${usagePercent}%)`;
+			const output_field = `// ${field}: used in ${stats.languagesUsing}/${meta.languageCount} (${usagePercent}%)`;
 
 			const output_named_type = ` -> ${namedType}`;
 			const output_split_into =
@@ -62,14 +62,14 @@ const emitStats: EmitStatsType = ({ map, types, config, totals, langs }) => {
 		.join("");
 
 	const output_lang_split_into =
-		config.type.splitLargeTypes && totals.total >= config.type.minItemsForSplit
-			? `// Language names split into ${Math.ceil(totals.total / config.type.itemsPerSegment)} segments (${config.type.itemsPerSegment} per segment)\n`
+		config.type.splitLargeTypes && meta.languageCount >= config.type.minItemsForSplit
+			? `// Language names split into ${Math.ceil(meta.languageCount / config.type.itemsPerSegment)} segments (${config.type.itemsPerSegment} per segment)\n`
 			: "";
 
-	const output_sample_names = `// Sample names: ${langs
-		.slice(0, 5)
-		.map((n) => `"${n}"`)
-		.join(", ")}${langs.length > 5 ? "..." : ""}\n\n`;
+	// const output_sample_names = `// Sample names: ${langs
+	// 	.slice(0, 5)
+	// 	.map((n) => `"${n}"`)
+	// 	.join(", ")}${langs.length > 5 ? "..." : ""}\n\n`;
 
 	return [
 		`// Field Statistics:\n`,
@@ -78,9 +78,8 @@ const emitStats: EmitStatsType = ({ map, types, config, totals, langs }) => {
 
 		//
 		`// Language Name Statistics:\n`,
-		`// Total languages: ${totals.total}\n`,
+		`// Total languages: ${meta.languageCount}\n`,
 		output_lang_split_into,
-		output_sample_names,
 	].join("");
 };
 
