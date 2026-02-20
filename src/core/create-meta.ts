613bbd1aab4804fdac66d4a63c2e804b2e492b4e
2qp <56475797+2qp@users.noreply.github.com>
2026-02-20 18:12:51 +0530

    feat(core): add `fieldCount` on `createMeta`
    - add `ref` param.
    - rename `length` to `languageCount`.

--- src/core/create-meta.ts
+++ src/core/create-meta.ts
@@ -1,23 +1,27 @@
 import type { Config } from "@/types/config.types";
 import type { LanguageData } from "@/types/lang.types";
+import type { Ref } from "./create-reference";
 
 type Meta = {
-	length: number;
+	languageCount: number;
+	fieldCount: number;
 };
 
 type CreateMetaParams<T extends LanguageData> = {
 	config: Config;
 	source: T;
+	ref: Ref;
 };
 
 type CreateMeta = <T extends LanguageData>(params: CreateMetaParams<T>) => Meta;
 
-const createMeta: CreateMeta = ({ source }) => {
+const createMeta: CreateMeta = ({ source, ref }) => {
 	//
 
 	const keys = Object.keys(source);
+	const size = ref.uidToField.size;
 
-	return { length: keys.length };
+	return { languageCount: keys.length, fieldCount: size };
 };
 
 export { createMeta };
