bfb43187d91b347b1e108af5f4c85fffee81d51b
2qp <56475797+2qp@users.noreply.github.com>
2026-02-20 21:50:19 +0530

    refactor(gen): add `FieldAnalysisArray`, `Meta`, `Ref` params
    - add `generic` `params`.
    - add drilled props to `generateDynamicTypes`.

--- src/gen/generate-dynamic-types.ts
+++ src/gen/generate-dynamic-types.ts
@@ -1,6 +1,4 @@
 import { processFields } from "./utils/process-fields";
-import { analyzeFields } from "@core/analyze-fields";
-import { createReference } from "@core/create-reference";
 import { emitAutogenHeader } from "@/emit/emit-autogen-header";
 import { emitLanguagePropertyTypeName } from "@/emit/emit-language-property-type-name";
 import { emitLanguageType } from "@/emit/emit-language-type";
@@ -11,56 +9,53 @@ import { emitTypesSection } from "@/emit/emit-types-sections";
 import { emitTypeSafeAccessors } from "@/emit/emit-typesafe-accessors";
 import { emitUtilityTypes } from "@/emit/emit-utility-types";
 
+import type { Meta } from "@core/create-meta";
+import type { Ref } from "@core/create-reference";
+import type { Field } from "@/types/branded.types";
 import type { Config } from "@/types/config.types";
+import type { FieldAnalysisArray } from "@/types/field.types";
+import type { Primitive } from "@/types/gen.types";
 import type { LanguageData } from "@/types/lang.types";
 
-type GenerateDynamicTypesParams<TSource extends Record<string, unknown> = LanguageData> = {
+type GenerateDynamicTypesParams<
+	TSource extends Record<string, unknown> = LanguageData,
+	TField extends string = Field,
+	TUnique extends Primitive = Primitive,
+> = {
 	config: Config;
 	source: TSource;
+	ref: Ref;
+	meta: Meta;
+	stats: FieldAnalysisArray<TField, TUnique>;
 };
 
 type GenerateDynamicTypesType = (params: GenerateDynamicTypesParams) => string;
 
-const generateDynamicTypes: GenerateDynamicTypesType = ({ config: base, source }) => {
+const generateDynamicTypes: GenerateDynamicTypesType = ({ config: base, source, meta, ref, stats }) => {
 	//
 
 	const config = { ...base, type: { ...base.type, secondary: { ...base.type.secondary, enabled: false } } };
 
-	const ref = createReference({ config, source });
-
-	const fieldStats = analyzeFields({ source, config, ref });
-
 	//
 
-	const keys = Object.keys(source);
-	const totalLanguages = keys.length;
-	const languageNames = keys.sort();
-
-	const fieldsArray = [...fieldStats].sort();
-
 	const fields = processFields({
-		fields: fieldsArray,
-		totalLanguages,
+		fields: stats,
 		config,
+		meta,
 		ref,
 	});
-
-	const newFieldStats = new Map(fields.updatedFields);
 
 	const name = config.type.naming.languageName;
 
 	// OUTPUTS
-	const output_header = emitAutogenHeader(config.core.name, config, {
-		size: fieldStats.size,
-		total: totalLanguages,
-	});
+	const output_header = emitAutogenHeader(config.core.name, config, meta);
 
 	const output_segments = emitSegmentSection(fields.allSegmentDefinitions);
 
 	const output_sorted_types = emitTypesSection(fields.generatedTypes, name);
 
 	const output_language_type = emitLanguageType({
-		stats: newFieldStats,
+		stats: fields.updatedFields,
 		types: fields.generatedTypes,
 		config,
 		ref,
@@ -71,7 +66,7 @@ const generateDynamicTypes: GenerateDynamicTypesType = ({ config: base, source }
 	const output_typesafe_accessors = emitTypeSafeAccessors(name);
 	// const output_validation_helpers = emitValidationHelpers(LANGUAGE_NAME);
 
-	const secondary = emitSecondaryTypes({ config: base, data: source, stats: fieldStats, ref });
+	const secondary = emitSecondaryTypes({ config: base, source, stats, ref, meta });
 
 	const output_typeNames = emitLanguagePropertyTypeName({ types: fields.generatedTypes, config });
 
@@ -94,15 +89,14 @@ const generateDynamicTypes: GenerateDynamicTypesType = ({ config: base, source }
 	if (config.type.showFieldStats) {
 		//
 
-		const stats = emitStats({
-			map: fieldStats,
+		const stats_output = emitStats({
+			stats,
 			types: fields.generatedTypes,
 			config: base,
-			langs: languageNames,
-			totals: { size: fieldStats.size, total: totalLanguages },
+			meta,
 		});
 
-		return [output, stats].join("");
+		return [output, stats_output].join("");
 	}
 
 	return output;
--- src/gen/generate-types.ts
+++ src/gen/generate-types.ts
@@ -5,23 +5,35 @@ import { resolvePath } from "@utils/resolve-path";
 import { writeFile } from "@utils/write-file";
 import { createReExports } from "@/transform/utils/create-re-exports";
 
+import type { Meta } from "@core/create-meta";
+import type { Ref } from "@core/create-reference";
+import type { Field } from "@/types/branded.types";
 import type { Config } from "@/types/config.types";
+import type { FieldAnalysisArray } from "@/types/field.types";
+import type { Primitive } from "@/types/gen.types";
 import type { LanguageData } from "@/types/lang.types";
 
-type GenerateTypesParams<TSource extends Record<string, unknown> = LanguageData> = {
+type GenerateTypesParams<
+	TSource extends Record<string, unknown> = LanguageData,
+	TField extends string = Field,
+	TUnique extends Primitive = Primitive,
+> = {
 	config: Config;
 	source: TSource;
+	ref: Ref;
+	meta: Meta;
+	stats: FieldAnalysisArray<TField, TUnique>;
 };
 
 type GenerateTypesType = (params: GenerateTypesParams) => Promise<void>;
 
-const generateTypes: GenerateTypesType = async ({ config, source }) => {
+const generateTypes: GenerateTypesType = async ({ config, source, meta, ref, stats }) => {
 	//
 
 	const outputDir = resolvePath(config.type.paths.outputDir);
 
 	try {
-		const typesOutput = generateDynamicTypes({ config: config, source });
+		const typesOutput = generateDynamicTypes({ config: config, source, meta, ref, stats });
 
 		await ensureDir(outputDir);
 		const outputPath = join(outputDir, config.type.out.fileName);
