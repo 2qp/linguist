289944486e434e3fda2bb0987715b868504562ce
2qp <56475797+2qp@users.noreply.github.com>
2026-02-22 19:01:49 +0530

    refactor(emit): add `types:` `OutputMap` param on `emitTypesSection`

--- src/emit/emit-types-sections.ts
+++ src/emit/emit-types-sections.ts
@@ -1,11 +1,7 @@
-import type { UID } from "@/types/branded.types";
-import type { GeneratedDefs } from "@/types/def.types";
 import type { Primitive } from "@/types/gen.types";
+import type { OutputMap } from "@/types/output.types";
 
-const emitTypesSection = <TUnique extends Primitive>(
-	types: Map<UID, GeneratedDefs<TUnique, string>>,
-	languageName: string,
-): string =>
+const emitTypesSection = <TUnique extends Primitive>(types: OutputMap<TUnique>, languageName: string): string =>
 	[...types.keys()]
 		.filter((tN) => tN !== languageName)
 		.map((uid) => `export type ${types.get(uid)?.type} = ${types.get(uid)?.typeDef};\n`)
