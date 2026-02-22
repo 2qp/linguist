e6a743e3f7f838eff3ab2a4e3c15b9056c8f28ff
2qp <56475797+2qp@users.noreply.github.com>
2026-02-22 21:00:01 +0530

    feat(transform/utils): add `createValueArray`

---
+++ src/transform/utils/create-value-array.ts
@@ -0,0 +1,53 @@
+import type { Config } from "@/types/config.types";
+import type { HomogeneousArray } from "@/types/utility.types";
+
+type CreateValueArrayParams<TSource extends Record<string, unknown>, TField extends keyof TSource[keyof TSource]> = {
+	source: TSource;
+	field: TField;
+	config: Config;
+};
+
+// type CreateValueArray = <
+// 	const TSource extends Record<string, unknown>,
+// 	const TField extends keyof TSource[keyof TSource],
+// >(
+// 	params: CreateValueArrayParams<TSource, TField>,
+// ) => HomogeneousArray<TSource, TField>;
+
+const createValueArray = <
+	const TSource extends Record<string, unknown>,
+	const TField extends keyof TSource[keyof TSource],
+>({
+	source,
+	field,
+}: CreateValueArrayParams<TSource, TField>): HomogeneousArray<TSource, TField> => {
+	//
+
+	const values = new Set();
+
+	for (const name of Object.keys(source) as (keyof TSource)[]) {
+		if (!name) continue;
+
+		const language = source[name];
+		if (!language) continue;
+
+		const element = language[field] as TSource[keyof TSource][TField];
+		if (!element) continue;
+
+		if (!Array.isArray(element)) {
+			values.add(element);
+			continue;
+		}
+
+		// const ar = element as unknown as T[keyof T][keyof T[keyof T]][keyof T[keyof T][keyof T[keyof T]]] & unknown[];
+
+		for (const item of element) {
+			values.add(item);
+		}
+	}
+
+	return [...values] as HomogeneousArray<TSource, TField>;
+};
+
+export { createValueArray };
+export type { CreateValueArrayParams };
