e185aee63dfee306d14ce57b423754e723066c6c
2qp <56475797+2qp@users.noreply.github.com>
2026-02-24 22:35:44 +0530

    feat(transform/utils): add `wrapAsConst`, `createConst`
    - add `FallbackForUnknownKeys<$>` on `Wrapper` type.

--- src/transform/utils/create-statements-utils.ts
+++ src/transform/utils/create-statements-utils.ts
@@ -1,11 +1,18 @@
 import { join } from "@utils/join";
 import { safeReplacer } from "@utils/safe-replacer";
 
-type Wrapper = "ReadonlyArray<$>" | "Partial<$>" | "$";
+type Wrapper = "ReadonlyArray<$>" | "Partial<$>" | "FallbackForUnknownKeys<$>" | "$";
 
 const getWrapped = <const TSource extends string[], TWrapper extends Wrapper>(
 	input: TSource,
 	wrapper: TWrapper = "$" as TWrapper,
 ) => safeReplacer(wrapper, "$" as const, join(input, " | "));
 
+const wrapAsConst = <const T extends string>(str: T) => `${str} as const;` as const;
+
+const createConst = <const TName extends string, const TValue extends string>(name: TName, value: TValue) =>
+	`const ${name} = ${value}` as const;
+
 export { getWrapped };
+
+export { createConst, wrapAsConst };
