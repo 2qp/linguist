4a0415d27e6b91459afc74a9500de01b9838a676
2qp <56475797+2qp@users.noreply.github.com>
2026-02-25 10:01:10 +0530

    feat(transform/utils): add `import` builder branch on `createStatementBuilder`

--- src/transform/utils/statement/create-statement-builder.ts
+++ src/transform/utils/statement/create-statement-builder.ts
@@ -1,9 +1,28 @@
+import { join } from "@utils/join";
+
 type CreateStatementBuilderParams = {};
 
 type CreateStatementBuilder = (params?: CreateStatementBuilderParams) => void;
 
-const createStatementBuilder: CreateStatementBuilder = () => {
-	return;
+const createStatementBuilder = () => {
+	//
+
+	const builder = {
+		//
+
+		import: () => ({
+			types: <const TTypes extends string[]>(types: TTypes) => ({
+				from: <const TPath extends string>(path: TPath) =>
+					`import type { ${join(types, ", ")} } from "${path}";` as const,
+			}),
+
+			values: <const TTypes extends string[]>(types: TTypes) => ({
+				from: <const TPath extends string>(path: TPath) => `import { ${join(types, ", ")} } from "${path}";` as const,
+			}),
+		}),
+	};
+
+	return builder;
 };
 
 export { createStatementBuilder };
