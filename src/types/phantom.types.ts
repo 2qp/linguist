f7f2ba24919d889bd605321136915c1459bbdb04
2qp <56475797+2qp@users.noreply.github.com>
2026-03-10 13:23:13 +0530

    chore(types): add  `WithVariant` utility on `phantom.types`

--- src/types/phantom.types.ts
+++ src/types/phantom.types.ts
@@ -2,4 +2,6 @@ import type { Phase } from "./core.types";
 
 type WithPhase<P extends Phase> = { _phase: P & {} };
 
-export type { WithPhase };
+type WithVariant<U, K extends U> = { _variant: K & {} };
+
+export type { WithPhase, WithVariant };
