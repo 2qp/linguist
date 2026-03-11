ae31d00163202d2a281eb5d8f60c6fe6508164d8
2qp <56475797+2qp@users.noreply.github.com>
2026-03-11 21:24:06 +0530

    chore(types): add `SegmentDefRecord`, `RecordToLiteral` on `segment.types`

--- src/types/segment.types.ts
+++ src/types/segment.types.ts
@@ -1,10 +1,75 @@
 import type { Capitalized, NameId, Primitive, QuoteString } from "./gen.types";
+import type { JoinUnion } from "./union.types";
 import type { UnionToChunks, UnionToChunksResult, UnionToString } from "./utils/union-to-string.util.types";
+
+type ElementToPrimitive<T> = T extends string
+	? string
+	: T extends number
+		? number
+		: T extends bigint
+			? bigint
+			: T extends boolean
+				? boolean
+				: T extends undefined
+					? undefined
+					: T extends null
+						? null
+						: T extends unknown
+							? `unknown`
+							: never;
 
 type SegmentDef<
 	T extends Primitive[][],
 	TName extends string,
-> = `const ${NameId<TName>} = [${`${QuoteString<T[number][number]>}, ${QuoteString<T[number][number]>}`}] as const;`;
+> = `const ${NameId<TName>} = [${`${QuoteString<ElementToPrimitive<T[number][number]>>}${string}`}] as const;`;
+
+type TypeLabel<V> = V extends string
+	? string
+	: V extends number
+		? number
+		: V extends boolean
+			? boolean
+			: V extends unknown[]
+				? `string[]`
+				: V extends null
+					? null
+					: V extends undefined
+						? undefined
+						: V extends unknown
+							? `unknown`
+							: never;
+
+type Widen<T> = T extends string
+	? string
+	: T extends number
+		? number
+		: T extends boolean
+			? boolean
+			: T extends readonly unknown[]
+				? unknown[]
+				: T extends null | undefined
+					? T
+					: T extends object
+						? { [K in keyof T]: Widen<T[K]> }
+						: T;
+
+type RequiredValue<V> = Exclude<V, undefined>;
+
+type ObjectLiteralString<
+	T extends Record<string, unknown>,
+	TOptions extends { partial: boolean },
+> = `{${JoinUnion<{ [K in keyof T & string]: `${K}${TOptions["partial"] extends true ? "?" : ""}:${TypeLabel<RequiredValue<T[K]>>}, ` }[keyof T & string]>}}`;
+
+type SegmentDefRecord<
+	T extends Record<string, unknown>[][],
+	TName extends string,
+	TOptions extends { partial: boolean } = { partial: false },
+> = `const ${NameId<TName>} = [${ObjectLiteralString<Widen<T[number][number]>, TOptions>}] as const;`;
+
+type RecordToLiteral<
+	T extends Record<string, unknown>,
+	TOptions extends { partial: boolean } = { partial: false },
+> = ObjectLiteralString<Widen<T>, TOptions>;
 
 type ChunksToDef<Chunks extends unknown[], TName extends string, Counter extends unknown[] = []> = Chunks extends [
 	infer First,
@@ -36,4 +101,12 @@ type SegmentChunkedDef<
 		: never
 	: `const ${Capitalized<TName>}_${number} = [${Primitive}, ${Primitive}] as const;`;
 
-export type { SegmentChunkedDef, SegmentDef };
+export type {
+	ElementToPrimitive,
+	ObjectLiteralString,
+	RecordToLiteral,
+	SegmentChunkedDef,
+	SegmentDef,
+	SegmentDefRecord,
+	Widen,
+};
