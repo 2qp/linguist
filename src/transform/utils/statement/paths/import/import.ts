491d8f448dc604dec1591073ca95f3d8d7b1ed7b
2qp <56475797+2qp@users.noreply.github.com>
2026-03-18 22:04:22 +0530

    feat(transform/utils): add `builder.import.lazy` on `createStatementBuilder`

--- src/transform/utils/statement/paths/import/import.ts
+++ src/transform/utils/statement/paths/import/import.ts
@@ -1,19 +1,28 @@
 import { join } from "@utils/join";
 
-import type { ToString } from "@/types/gen.types";
-import type { From, ImportableType, SL } from "@/types/statement.types";
+import type { Primitive } from "@/types/gen.types";
+import type { ImportableType, SL } from "@/types/statement.types";
 
 const importBuilder = () => ({
 	types: <const TStrict extends ImportableType[], const TLoose extends string[]>(...args: SL<TStrict, TLoose>) => ({
-		from: <const TPaths, const TPath extends keyof TPaths>(...[paths, path]: From<TPaths, TPath>) => ({
-			build: () =>
-				`import type { ${join([...args[0], ...args[1]], ", ")} } from "${paths[path] as ToString<TPaths[TPath]>}";` as const,
+		from: <const TPaths extends string[]>(...paths: TPaths) => ({
+			build: () => `import type { ${join([...args[0], ...args[1]], ", ")} } from "${join(paths, "")}";` as const,
 		}),
 	}),
 
 	values: <const TValues extends string[]>(values: TValues) => ({
-		from: <const TPaths, const TPath extends keyof TPaths>(...[paths, path]: From<TPaths, TPath>) => ({
-			build: () => `import { ${join(values, ", ")} } from "${paths[path] as ToString<TPaths[TPath]>}";` as const,
+		from: <const TPaths extends string[]>(...paths: TPaths) => ({
+			build: () => `import { ${join(values, ", ")} } from "${join(paths, "")}";` as const,
+		}),
+	}),
+
+	lazy: () => ({
+		values: () => ({
+			from: <const TPaths extends string[]>(paths: TPaths) => ({
+				then_: <const TThen extends Primitive>(then: TThen) => ({
+					build: () => `import('${join(paths, "")}').then(({ ${then} }) => ${then})` as const,
+				}),
+			}),
 		}),
 	}),
 });
