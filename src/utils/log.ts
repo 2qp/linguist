0b879e70cdcc4db59f40b76751f43be91996a49a
2qp <56475797+2qp@users.noreply.github.com>
2026-03-24 14:02:09 +0530

    feat(utils): add `log`

---
+++ src/utils/log.ts
@@ -0,0 +1,11 @@
+const log = {
+	info: (...args: unknown[]) => console.info(...args),
+
+	warn: (...args: unknown[]) => console.warn(...args),
+
+	error: (...args: unknown[]) => console.error(...args),
+
+	debug: (...args: unknown[]) => console.debug(...args),
+};
+
+export { log };
