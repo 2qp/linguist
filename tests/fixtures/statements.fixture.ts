e4457de47eb50f681ba8d9cc8432021efc8e1e20
2qp <56475797+2qp@users.noreply.github.com>
2026-03-26 01:03:39 +0530

    test(fixtures): add `data.paths.typesDir` and `type.out.usage` on `dummyStmtPaths`

--- tests/fixtures/statements.fixture.ts
+++ tests/fixtures/statements.fixture.ts
@@ -3,9 +3,19 @@ import { createPartialConfig } from "./utils";
 import type { Config } from "@/types/config.types";
 
 const dummyStmtPaths = createPartialConfig({
+	data: {
+		paths: {
+			typesDir: "TYPES_DIR",
+		},
+	},
 	type: {
 		out: {
 			common: {
+				dir: { alias: "OUT_DIR" },
+				file: { name: "FILE_NO_EXT" },
+			},
+
+			usage: {
 				dir: { alias: "OUT_DIR" },
 				file: { name: "FILE_NO_EXT" },
 			},
