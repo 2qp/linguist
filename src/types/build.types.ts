90951a336abf9e4cb2c1f0e8bd2fe890ba8809bd
2qp <56475797+2qp@users.noreply.github.com>
2026-04-15 19:35:52 +0530

    chore(type): add `Upstream`, `WorkflowRun` on `BuildInfo`

--- src/types/build.types.ts
+++ src/types/build.types.ts
@@ -2,6 +2,22 @@ type BuildInfo = {
 	source: string;
 	hash: string;
 	generated_at: string;
+
+	upstream?: Upstream;
+	workflow_run?: WorkflowRun;
+};
+
+type Upstream = {
+	updated_at: Date;
+};
+
+type WorkflowRun = {
+	repository: string;
+	run_attempt: number;
+	run_id: number;
+	run_number: number;
+	trigger: string;
+	workflow_id: string;
 };
 
 export type { BuildInfo };
