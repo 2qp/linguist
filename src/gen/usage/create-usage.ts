180b39775dc126581c4d47e1007f641754cd34fb
2qp <56475797+2qp@users.noreply.github.com>
2026-04-19 21:54:34 +0530

    feat(gen/usage): add generation of `usage` fields
    - add `Generator` type on `CreateUsage`.
    - add same type `field` filter.

--- src/gen/usage/create-usage.ts
+++ src/gen/usage/create-usage.ts
@@ -1,8 +1,11 @@
 import { getUsageName } from "./get-usage-name";
 import { createFieldSet } from "@gen/utils/create-field-set";
+import { isNullish } from "@utils/guards";
 
+import type { Generator } from "@gen/types";
 import type { Field } from "@/types/branded.types";
 import type { Config } from "@/types/config.types";
+import type { Primitive } from "@/types/gen.types";
 import type { LanguageData } from "@/types/lang.types";
 
 type CreateUsageParams = {
@@ -10,20 +13,24 @@ type CreateUsageParams = {
 	config: Config;
 };
 
-type CreateUsage = (params: CreateUsageParams) => Map<Field, Record<string, ReadonlyArray<string>>>;
+type CreateUsage = Generator<Map<Field, Record<string, ReadonlyArray<Primitive>>>>;
 
-const createUsage: CreateUsage = ({ data, config }) => {
+const createUsage: CreateUsage = ({ source, config, stats: _stats }) => {
 	//
 
-	const fieldSet = createFieldSet({ source: data, config });
+	const stats = new Map(_stats);
+
+	const fieldSet = createFieldSet({ source, config });
 	const fields = [...fieldSet].sort();
 
-	const languages = Object.values(data);
+	const languages = Object.values(source);
 
 	const fieldAnalyses = fields.map((field) => {
-		const idsSet = new Set<string>();
-		const namesSet = new Set<string>();
-		const typeSet = new Set<string>();
+		//
+
+		const isArray = stats.get(field)?.isArray;
+
+		const map = new Map<string, Set<Primitive>>(fields.map((field) => [field, new Set<Primitive>()]));
 
 		for (const lang of languages) {
 			const value = lang[field];
@@ -32,20 +39,41 @@ const createUsage: CreateUsage = ({ data, config }) => {
 
 			if (!isValid) continue;
 
-			idsSet.add(lang.language_id as string);
-			namesSet.add(lang.name as string);
-			typeSet.add(lang.type as string);
+			for (const key of fields) {
+				//
+
+				if (isNullish(key)) continue;
+				if (key === field) continue;
+
+				const value = lang[key];
+
+				const values: Primitive[] = Array.isArray(value) ? value : [value];
+				const nonNullishValues = values.filter((v) => !isNullish(v));
+
+				const exist = map.get(key);
+
+				if (isNullish(exist)) {
+					map.set(key, new Set(nonNullishValues));
+					continue;
+				}
+
+				if (!nonNullishValues.length) continue;
+
+				map.set(key, new Set([...exist, ...nonNullishValues]));
+			}
 		}
 
-		const language_id_with = getUsageName({ left: "language_id", right: field });
-		const name_with = getUsageName({ left: "name", right: field });
-		const type_with = getUsageName({ left: "type", right: field });
+		const usageNames = fields
+			.filter((key) => key !== field)
+			.map((key) => {
+				const usageKey = getUsageName({ left: key, right: field });
 
-		const withStats = {
-			[language_id_with]: [...idsSet],
-			[name_with]: [...namesSet],
-			[type_with]: [...typeSet],
-		} as const;
+				const values = isArray ? [...(map.get(key) || [])].flat() : [...(map.get(key) || [])];
+
+				return [usageKey, values];
+			});
+
+		const withStats = Object.fromEntries(usageNames);
 
 		return [field, withStats] as const;
 	});
